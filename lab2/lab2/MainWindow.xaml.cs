388c35647c066c74c196841541a729a1065cbea7
ruslan3-47 <vip.artyxa@gmail.com>
2024-10-30 18:39:16 +0300

    final...

---
+++ lab2/lab2/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+
+namespace lab2
+{
+
+    public partial class MainWindow : Window
+    {
+        private Matrix matrix1;
+        private Matrix matrix2;
+        private Matrix result;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateMatrix_Click(object sender, RoutedEventArgs e)
+        {
+            int rows = int.Parse(Rows.Text);
+            int columns = int.Parse(Columns.Text);
+
+            matrix1 = new Matrix(rows, columns);
+            matrix2 = new Matrix(rows, columns);
+            CreateGrid(Matrix1grid, matrix1);
+            CreateGrid(Matrix2grid, matrix2);
+        }
+        private static void CreateGrid(Grid grid,Matrix matrix)
+        {
+            grid.Children.Clear();
+            grid.RowDefinitions.Clear();
+            grid.ColumnDefinitions.Clear();
+            
+            for (int i = 0; i < matrix.Row;i++) 
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0;i<matrix.Col;i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0;i<matrix.Row;i++)
+            {
+                for (int j = 0; j < matrix.Col;j++)
+                {
+                    var textbox = new TextBox
+                    {
+                        Text = "0",
+                        VerticalAlignment = VerticalAlignment.Center,
+                        HorizontalAlignment = HorizontalAlignment.Center,
+                        Width = 50,
+                        Height = 30,
+                        Tag = new Tuple<int,int>(i,j)
+                    };
+                    textbox.TextChanged += (s, e) =>
+                    {
+                        if (string.IsNullOrEmpty(textbox.Text))
+                        {
+                            textbox.Text = "";
+                            return;
+                        }
+                        if (int.TryParse(textbox.Text, out int value))
+                        {
+                            var index = (Tuple<int, int>)textbox.Tag;
+                            matrix[index.Item1, index.Item2] = value;
+                        }
+                        else
+                        {
+                            var index = (Tuple<int, int>)textbox.Tag;
+                            textbox.Text = matrix[index.Item1,index.Item2].ToString();
+                        }
+                    };
+                    Grid.SetRow(textbox, i);
+                    Grid.SetColumn(textbox, j);
+                    grid.Children.Add(textbox);
+                }
+            }
+        }
+
+        private void Sum_Click(object sender, RoutedEventArgs e)
+        {
+            if (matrix1 != null && matrix2 != null)
+            {
+                try
+                {
+                    result = matrix1 + matrix2;
+                    MessageBox.Show(result.ToString(), "Та-дам");
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show("Ошибка размерности");
+                }
+            }
+            else
+            {
+                MessageBox.Show("матрицы небыли созданы");
+            }
+        }
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            if (matrix1 != null && matrix2 != null)
+            {
+                try
+                {
+                    result = matrix1 * matrix2;
+                    MessageBox.Show(result.ToString(), "Та-дам");
+                }
+                catch(ArgumentException)
+                {
+                    MessageBox.Show("Ошибка");
+                }
+            }
+            else
+            {
+                MessageBox.Show("матрицы небыли созданы");
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (result != null)
+            {
+                SaveFileDialog save = new SaveFileDialog
+                {
+                    Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*",
+                    DefaultExt = "csv"
+                };
+                if(save.ShowDialog() == true)
+                {
+                    result.saveCSV(save.FileName);
+                    MessageBox.Show("Матрица сохранена");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Результирующая матрица не была создана");
+            }
+        }
+    }
+    }
---
+++ lab2/lab2/Matrix.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Matrix
+    {
+        public int Row {  get; set; }
+        public int Col { get; set; }
+        public int[,] Data;
+        public Matrix(int row,int column) {
+            Row = row;
+            Col = column;
+            Data = new int[row,column];
+        }
+        public int this[int row, int column]
+        {
+            get
+            {
+                if (row < 0 || row >= Row || column < 0 || column >= Col)
+                    throw new ArgumentException("Индексы выходят за границы матрицы.");
+                return Data[row,column];
+            }
+            set
+            {
+                if (row < 0 || row >= Row || column < 0 || column >= Col)
+                    throw new ArgumentException("Индексы выходят за границы матрицы.");
+                Data[row,column] = value;
+            }
+        }
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            if(a.Col != b.Col || a.Row != b.Row)
+            {
+                throw new ArgumentException("Введите корректные размерности");
+            }
+            Matrix result = new Matrix(a.Row,a.Col);
+            for (int i = 0; i < a.Row; i++) 
+                  { 
+                    for (int j = 0; j < a.Col; j++)
+                    {
+                        result[i, j] = a[i, j] + b[i, j];
+                    }
+                }
+            return result;
+        }
+        public static Matrix operator *(Matrix a, Matrix b)
+        {
+            if (a.Col != b.Row)
+            {
+                throw new ArgumentException("Введите корректные размерности");
+            }
+            Matrix result = new Matrix(a.Row, b.Col);
+            for (int i = 0;i < a.Row;i++)
+            {
+                for (int j = 0;j < b.Col;j++)
+                {
+                    for (int k = 0;k < a.Col;k++)
+                    {
+                        result[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0;i< Row; i++) 
+            {
+                for (int j = 0;j< Col;j++)
+                {
+                    result += Data[i, j] + "\t";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+        public void saveCSV(string filepath) 
+        {
+            using (StreamWriter writer = new StreamWriter(filepath))
+            {
+                for (int i = 0; i < Row; i++)
+                {
+                    for (int j = 0; j < Col; j++)
+                    {
+                        writer.Write(Data[i, j]);
+                        if (j < Col - 1)
+                        {
+                            writer.Write(";");
+                        }
+
+                    }
+                    writer.Write("\n");
+                }
+                writer.WriteLine();
+            }
+        }
+    }
+}
